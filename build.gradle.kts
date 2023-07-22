fd71ac48ed8bb740c6a74fc084c48ab3fef007f2
osawa-koki <github@osawa.dev>
2023-07-22 23:06:56 +0900

    リファクタリング。

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
-    id("java")
+  id("java")
 }
 
 group = "org.example"
 version = "1.0.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("org.junit:junit-bom:5.9.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 buildDir = File("./build")
